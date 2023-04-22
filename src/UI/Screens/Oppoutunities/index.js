2772173ac2c77ca5a51846ea851a1d295de45e0a
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-22 16:41:04 +0530

    commit-4

--- src/UI/Screens/Oppoutunities/index.js
+++ src/UI/Screens/Oppoutunities/index.js
@@ -4,54 +4,104 @@ import pic1 from "../../../Assets/Oppourtunities/oppourtunity1.jpeg"
 const Oppourtunities = () => {
   return (
     <>
-        <div class="about-section">
-    <h1>Opportunities</h1>
-    <p>List of upcoming internships, Jobs and Competitions.</p>
-  </div>
-  
-  <h2 className='text-center mt-5'></h2>
-  <div class="row">
-    <div class="column">
-      <div class="card">
-        <img src={pic1} alt="Coding Contest" className='w-full'/>
-        <div class="containerx">
-          <h2>Coding Scholarship Test</h2>
-          <p class="title">Coding Ninja</p>
-          <p><a href="https://bit.ly/3UREFKm">Registeration Link</a></p>
-          <p><button class="button">Contact Us</button></p>
-        </div>
-      </div>
-    </div>
-  
-    <div class="column">
-      <div class="card">
-        <img src={pic1} className='w-full'/>
-        <div class="containerx">
-          <h2>Graphic Designer</h2>
-          <p class="title">Marcos pvt ltd.</p>
-          <p>Looking for a Graphic Designer with minimum experience of 1 year. Can send the Resume to the email ID attached below.</p>
-          <p>marcos@gmail.com</p>
-          <p><a href="https://bit.ly/3UREFKm">Registeration Link</a></p>
-          <p><button class="button">Contact Us</button></p>
-        </div>
-      </div>
-    </div>
-  
-    <div class="column">
-      <div class="card">
-        <img src={pic1} alt="John" className='w-full'/>
-        <div class="containerx">
-          <h2>Backend Developer</h2>
-          <p class="title">Codehelp</p>
-          <p>Looking for an Intern. Who is good at backend Development. No more prior experience is required. </p>
-          <p>Striver@gmail.com</p>
-          <p><a href="https://bit.ly/3UREFKm">Registeration Link</a></p>
-          <p><button class="button">Contact Us</button></p>
-        </div>
-      </div>
-    </div>
-    
-  </div>
+        <h1 class="mt-10 text-center text-4xl font-bold text-blue-600 underline">Research Papers</h1>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 pt-10 pb-16 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div class="p-4 lg:w-1/3">
+              <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Harsh Choudhary</h1>
+                <p class="leading-relaxed mb-3 text-1xl font-bold">Object detection using machine learning</p>
+                <p class="items-center text-black">Object detection algorithms typically leverage machine learning or deep learning to produce meaningful results. When humans look at images or video, we can recognize and locate objects of interest within a matter of moments. The goal of object detection is to replicate this intelligence using a computer.</p>
+                <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+              </div>
+            </div>
+            <div class="p-4 lg:w-1/3">
+                <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                  <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Mrinmoy Saikia</h1>
+                  <p class="leading-relaxed mb-3 text-1xl font-bold">An Analysis of Carbon-Reduction Pledges</p>
+                  <p class="items-center text-black">We study carbon-reduction pledges of publicly traded U.S. oil exploration and production (E&P) companies. Of the 69 firms in the sample, 18 have committed to net-zero carbon targets, 14 have announced significant emissions cuts </p>
+                  <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+                </div>
+              </div>
+              <div class="p-4 lg:w-1/3">
+                <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                  <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Divesh Dhanuk Raj</h1>
+                  <p class="leading-relaxed mb-3 text-1xl font-bold">Is Q-Learning Minimax Optimal?</p>
+                  <p class="items-center text-black">Q-learning, which seeks to learn the optimal Q-function of a Markov decision process (MDP) in a model-free fashion, lies at the heart of reinforcement learning. When it comes to the synchronous setting (such that independent samples for all state</p>
+                  <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
+      <hr className='py-10'/>
+
+      <h1 class="mt-5  text-center text-4xl text-blue-600 font-bold underline">Projects</h1>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 pt-10 pb-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div class="p-4 lg:w-1/3">
+              <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Mrinmoy Saikia</h1>
+                <p class="leading-relaxed mb-3 text-1xl font-bold">E Commerce Website</p>
+                <p class="items-center text-black">The standard definition of E-commerce is a commercial transaction which is happened over the internet. Online stores like Amazon, Flipkart, Shopify, Myntra, Ebay, Quikr, Olx are examples of E-commerce websites.</p>
+                <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+              </div>
+            </div>
+            <div class="p-4 lg:w-1/3">
+                <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                  <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Harsh Choudhary</h1>
+                  <p class="leading-relaxed mb-3 text-1xl font-bold">Develop a chatbot using python</p>
+                  <p class="items-center text-black">A chatbot or chatterbot is a software application used to conduct an online chat conversation via text or text-to-speech, in lieu of providing direct contact with a live human agent. </p>
+                  <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+                </div>
+              </div>
+              <div class="p-4 lg:w-1/3">
+                <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                  <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Divesh Dhanuk Raj</h1>
+                  <p class="leading-relaxed mb-3 text-1xl font-bold">Stock prediction using machine learning</p>
+                  <p class="items-center text-black">A stock market is a public market where you can buy and sell shares for publicly listed companies. The stocks, also known as equities, represent ownership in the company. The stock exchange is the mediator that allows the buying and selling of shares. </p>
+                  <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
+
+      <hr className='py-10'/>
+
+      <h1 class="mt-6 font-bold text-center text-4xl text-bold text-blue-600 underline">Internships</h1>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 pt-16 pb-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div class="p-4 lg:w-1/3">
+              <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Google India</h1>
+                <p class="leading-relaxed mb-3 text-1xl font-bold">Software Development Intern</p>
+                <p classS="items-center text-black">Google India is the subsidiary of Google LLC that operates in the Indian subcontinent, with headquarters in Hyderabad and other offices in major cities. The company has been actively involved in promoting digital literacy and supporting initiatives to improve access to technology and information in the country. Overall, Google India plays a significant role in the digital landscape of India.</p>
+                <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+              </div>
+            </div>
+            <div class="p-4 lg:w-1/3">
+                <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                  <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Microsoft India</h1>
+                  <p class="leading-relaxed mb-3 text-1xl font-bold">Software Engineer Intern</p>
+                  <p class="items-center text-black">Microsoft India is the subsidiary of Microsoft Corporation that operates in India, with its headquarters in the city of Hyderabad. The company is involved in providing software, hardware, cloud computing, and enterprise solutions to businesses and consumers in India. Microsoft India has also been actively involved in promoting digital literacy and supporting various initiatives to empower individuals and organizations with technology.</p>
+                  <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+                </div>
+              </div>
+              <div class="p-4 lg:w-1/3">
+                <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                  <h1 class="title-font sm:text-4xl text-xl font-medium text-[#0057FF] mb-3">Tata Consultancy Limited</h1>
+                  <p class="leading-relaxed mb-3 text-1xl font-bold">Backend Development Intern</p>
+                  <p class="items-center text-black">Tala Consultancy Limited is a UK-based management consulting firm that provides advisory services to clients in the private and public sectors. The company specializes in strategy, transformation, and change management, and has worked with clients in industries such as financial services, healthcare, and energy.</p>
+                  <button class="flex mx-auto mt-16 text-white bg-[#0057FF] border-0 py-2 px-8 focus:outline-none hover:bg-blue-700 rounded text-lg">Contribute</button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </section>
     </>
   )
 }
